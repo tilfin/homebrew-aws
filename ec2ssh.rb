9acccf179a0671b92c63aadc3e9d7f1468baad1c
Toshimitsu Takahashi <toshi@tilfin.com>
2017-09-12 18:08:58 +0900

    Bump v1.0.1

--- ec2ssh.rb
+++ ec2ssh.rb
@@ -1,8 +1,8 @@
 class Ec2ssh < Formula
   homepage "https://github.com/tilfin/homebrew-aws"
   url "https://raw.githubusercontent.com/tilfin/homebrew-aws/master/bin/ec2ssh"
-  sha256 "df54b690be920eaf67d16f9761e03b72fb02f46fe69aa5f4e0abb14af6b3034a"
-  version "1.0"
+  sha256 "35f22185409f4f894b9f5bb33d19d23cc7b0681db9b9517d8735b30dde1d8db3"
+  version "1.0.1"
 
   depends_on "jq"
 
