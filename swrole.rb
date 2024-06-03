f974e2b6dee2346312177215c0152b550de2aaf9
Toshimitsu Takahashi <toshi@tilfin.com>
2024-06-03 10:39:45 +0900

    update swrole formula

--- swrole.rb
+++ swrole.rb
@@ -1,7 +1,7 @@
 class Swrole < Formula
   homepage "https://github.com/tilfin/homebrew-aws"
-  url "https://raw.githubusercontent.com/tilfin/homebrew-aws/master/bin/swrole"
-  sha256 "ecd4b5ce743910245f7d5bca8f7487b9fadd6f01bcc4a7ad10c17b368eabdc76"
+  url "https://raw.githubusercontent.com/tilfin/homebrew-aws/master/bin/swrole-1.1.tar.gz"
+  sha256 "151a772bd0475af3d29a3320fe23c9eec085dd29dbacd6409fc9d1c496c67561"
   version "1.1"
 
   depends_on "jq"
